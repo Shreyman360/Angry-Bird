11bb06d22c9269b7317b219e9ecae691da42a0cd
Shreyman360 <vkp5@yahoo.com>
2021-01-05 17:42:48 -0500

    Create A Chain Class

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,15 +37,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6= new Log(230,180,80,PI/2);
 
+    chain=new Chain(bird.body,log6.body);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -60,4 +62,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+
+    chain.display();
+
 }
